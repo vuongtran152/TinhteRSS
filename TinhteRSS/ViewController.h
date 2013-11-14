6c7d2b4aabe48a6ec33b29b14ed5defdcbaa38f8
Tran Cong Vuong <han@Trans-MacBook-Pro.local>
2013-11-15 03:16:17 +0700

    Initial Commit

---
+++ TinhteRSS/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TinhteRSS
+//
+//  Created by Tran Cong Vuong on 11/15/13.
+//  Copyright (c) 2013 Tran Cong Vuong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ TinhteRSS/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TinhteRSS
+//
+//  Created by Tran Cong Vuong on 11/15/13.
+//  Copyright (c) 2013 Tran Cong Vuong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
